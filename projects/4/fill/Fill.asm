6fd666a3b57de6558d99ef66dc5d21ade8785ab3
mmucahitOt <m.mucahit0@gmail.com>
2025-02-16 11:22:19 +0300

    Project 4 is completed. *Fill.asm, *Mult.asm

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,69 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// Initialize variables
+    @SCREEN     // Set base address of screen memory map
+    D=A
+    @addr
+    M=D        // addr = SCREEN (16384)
+    
+    @8192      // Total number of 16-bit words to fill (256 rows * 32 words per row)
+    D=A
+    @n
+    M=D        // n = 8192
+
+(LOOP)
+    @KBD       // Check keyboard input
+    D=M
+    
+    @BLACK     // If key pressed (D > 0), jump to BLACK
+    D;JGT
+    
+    @WHITE     // Else jump to WHITE
+    0;JMP
+
+(BLACK)
+    @color
+    M=-1       // color = -1 (1111111111111111 in binary)
+    @DRAW
+    0;JMP
+
+(WHITE)
+    @color
+    M=0        // color = 0 (0000000000000000 in binary)
+    @DRAW
+    0;JMP
+
+(DRAW)
+    @n         // if n <= 0, reset and restart
+    D=M
+    @RESET
+    D;JLE
+
+    @color     // Get current color
+    D=M
+    @addr      // Get current address
+    A=M        // Select address to modify
+    M=D        // Set pixels at current address
+
+    @addr      // Increment address
+    M=M+1
+    @n         // Decrement counter
+    M=M-1
+    
+    @DRAW      // Continue drawing
+    0;JMP
+
+(RESET)
+    @SCREEN    // Reset address to start of screen
+    D=A
+    @addr
+    M=D
+    
+    @8192      // Reset counter
+    D=A
+    @n
+    M=D
+    
+    @LOOP      // Return to main loop
+    0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    @R2
+    M=0     // Initialize R2 to 0
+    
+    @R1
+    D=M
+    @END
+    D;JLE   // If R1 <= 0, goto END
+
+(LOOP)
+    @R0
+    D=M     // D = R0
+    @R2
+    M=M+D   // R2 += R0
+    
+    @R1
+    M=M-1   // R1--
+    D=M
+    @LOOP
+    D;JGT   // If R1 > 0, continue loop
+    
+(END)
+    @END
+    0;JMP   // Infinite loop to end program
+
